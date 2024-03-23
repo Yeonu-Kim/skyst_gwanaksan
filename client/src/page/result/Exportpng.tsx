c3999edda7e3917be71abc59497189e2f30634ba
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-03-24 06:18:48 +0900

    [fix] file name of capture

--- client/src/page/result/Exportpng.tsx
+++ client/src/page/result/Exportpng.tsx
@@ -2,6 +2,7 @@ import html2canvas from "html2canvas";
 
 const exportPNG = () => {
   html2canvas(document.body, {
+    backgroundColor: "#10162E",
     scale: window.devicePixelRatio
   })
   .then(canvas => downloadImage(canvas.toDataURL('image/png')))
@@ -12,7 +13,7 @@ const downloadImage = (uri: string) => {
   const fakeLink = window.document.createElement("a");
   document.body.appendChild(fakeLink);
   fakeLink.href = uri;
-  fakeLink.setAttribute('download', 'full-page.png');
+  fakeLink.setAttribute('download', 'result.png');
   fakeLink.click();
   document.body.removeChild(fakeLink);
 }
