9f02132c7d316063dff866e0195fd3df6d11c08d
yeonuKim <ywk0524@snu.ac.kr>
2024-03-23 15:08:27 +0900

    [feat] Make client router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,32 @@
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Main Page</div>
+  },
+  {
+    path: '/face',
+    element: <div>face</div>,
+  },
+  {
+    path: '/keyword',
+    element: <div>keyword</div>
+  },
+  {
+    path: '/result',
+    element: <div>result</div>,
+  }
+]);
+
+
 function App() {
   return (
     <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
