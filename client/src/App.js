5af564ef39c0f2155b54119c3eb76ebba608eef7
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-03-23 16:25:45 +0900

    [feat] common/button,title,navBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
+import React from 'react';
+import './page/common/Global.css'; // Import the CSS file
+import {NavBar,Footer} from './page/common/NavBar';
+import Button from './page/common/Button';
+import {Title, SubTitle, Paragraph} from './page/common/Title';
 function App() {
   return (
     <div>
+      <Button/>
     </div>
   );
 }
---
+++ client/src/page/common/Button.js
@@ -0,0 +1,13 @@
+
+
+
+  import React from 'react';
+  import styles from './Button.module.css'; 
+  
+  const Button = () => {
+    return (
+        <button className={styles.button}>Click me</button>
+    );
+  };
+  
+export default Button;
