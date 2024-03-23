6630e6e118c4c196577f7cb022d033f88320d2be
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-03-23 17:27:59 +0900

    [feat] btn design

--- client/src/page/common/Button.js
+++ client/src/page/common/Button.js
@@ -1,13 +1,31 @@
-
-
-
-  import React from 'react';
-  import styles from './Button.module.css'; 
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Button.module.css'; 
   
-  const Button = () => {
+  const Button = ({text, onClickColor, originalColor}) => {
+    const [isClicked, setIsClicked] = useState(false);
+    const handleClick = () => {
+      setIsClicked(current => !current); 
+    };
+    const buttonClasses = `${styles.button} ${isClicked ? styles.clicked : ''}`;
+    const buttonStyle = {
+      backgroundColor: isClicked ? onClickColor : originalColor,
+    };
     return (
-        <button className={styles.button}>Click me</button>
+      <button 
+      className={buttonClasses} 
+      onClick={handleClick} 
+      style={buttonStyle}
+      on
+    >  
+        <span className={styles.btnText}>{text}</span>
+    </button>
     );
   };
-  
+
+  Button.propTypes = {
+    text: PropTypes.string.isRequired,
+    onClickColor: PropTypes.string.isRequired,
+    backgroundColor: PropTypes.string.isRequired,
+  };
 export default Button;
