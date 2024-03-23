066b76d74f77503eaaaebaad0fc37629bb759aee
Junebum Ryoo <96vit_b@snu.ac.kr>
2024-03-23 21:43:54 +0900

    Image Similarity Function Enhanced

--- server/routes/keywords.py
+++ server/routes/keywords.py
@@ -9,7 +9,7 @@ router = APIRouter()
 @router.post("/")
 async def calculate_keywords(user_keywords: Keywords):
     df = pd.read_pickle("***REMOVED***/characters.pkl")
-    df.drop(columns=["image", "embedding"], inplace=True)
+    df.drop(columns=["embedding"], inplace=True)
     df["score"] = pd.Series([0] * len(df), index=df.index)
     for field in Keywords.model_fields:
         df["score"] += df[field].apply(lambda x: x in getattr(user_keywords, field))
